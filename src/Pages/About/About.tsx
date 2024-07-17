a20507d0a10d2f1e1f79f1b059fb0ed4f22f8fec
Lucas Gonzalez <lucas.gonzalez.espinosa@outlook.com>
2024-07-17 16:51:17 -0400

    chore: Update About page heading animation and add certifications stack component

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -3,8 +3,10 @@ import TechnologiesStack from "./Components/TechnologiesStack/TechnologiesStack"
 export default function About() {
 	return (
 		<section className="px-5 items-center mt-10 sm:px-20 lg:px-44">
-			<h1 className="font-bold text-5xl text-center mb-12">Acerca de mi</h1>
-			<p className="text-left text-wrap mb-14 p-10" data-aos="fade-up-left">
+			<h1 className="font-bold text-5xl text-center mb-12" data-aos="fade-up" data-aos-duration="1000">
+                Acerca de mi
+            </h1>
+			<p className="text-left text-wrap mb-14 p-10" data-aos="fade-up" data-aos-duration="1000">
 				<span className="text-xl font-bold text-orange-500">Especialista</span> en Tecnología de la Información con certificación en <span className=" font-bold text-red-300 text-xl">Ciberseguridad</span> para sistemas de control industrial.
 				Poseo una sólida experiencia en <span className="text-blue-300 font-bold text-xl">desarrollo de software</span>, complementada con habilidades en gestión de redes y soporte técnico.
 				Destaco por mi enfoque proactivo en la resolución de problemas y mi dedicación a la mejora continua en el ámbito de la ciberseguridad.
@@ -13,7 +15,7 @@ export default function About() {
 				Mi responsabilidad y compromiso con la calidad se reflejan en cada proyecto que emprendo,
 				siempre con el <span className="font-bold text-xl text-green-400">objetivo</span> de fortalecer la seguridad y eficiencia de los sistemas tecnológicos.
 			</p>
-
+ 
             <TechnologiesStack />
 		</section>
 	)    
---
+++ src/Pages/About/Components/CertificationsStack/CertificationsStack.tsx
@@ -0,0 +1,5 @@
+export default function CertificationsStack() {
+    return (
+        <div className=""></div>
+    )
+}
